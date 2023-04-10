38ce79a6c961338e5f30da5f6f87250b085bcf7c
mageshcoder <codermagesh@gmail.com>
2023-04-10 11:28:20 +0530

    Navigation fixed for mobile

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -59,9 +59,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b00e05657fa22c5f40c3ffe7d49d3511",
-"/": "b00e05657fa22c5f40c3ffe7d49d3511",
-"main.dart.js": "270207a31efb84947e3df972fbaf5137",
+"index.html": "ff23f3dc347cf25dc4546d416099ca2b",
+"/": "ff23f3dc347cf25dc4546d416099ca2b",
+"main.dart.js": "d39810f2bf1033c573230291f21acbbb",
 "manifest.json": "41c0fd832a62ef2d79da185db21180e9",
 "version.json": "397b325241089b82fd8ccb3d7201c781"
 };
